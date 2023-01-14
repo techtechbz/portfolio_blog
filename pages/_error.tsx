05cfe13cca63bd9306c96445a4419856cf9cdfe5
7kuma <neus0wch1ck@gmail.com>
2023-01-14 23:01:49 +0900

    Adjust site appearance of site.

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -21,7 +21,7 @@ export default function Error({heading, description}: ErrorPageProps) {
 
 Error.getInitialProps = ({ res, err }: NextPageContext) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  if (statusCode === 503) return { title: "503エラー", heading: "現在アクセスが集中しています。", description: "少し時間をおいてからリロードしてください。"}
+  if (statusCode === 503) return { title: "503エラー", heading: "現在アクセスが集中しています。", description: "5分ほど時間をおいてからリロードしてください。"}
   if (typeof statusCode === "number") return { title: "Error Occured", heading: "このページは現在表示できません。", description: `サーバー内部でエラーが発生しました。\nエラーコード : ${statusCode}`}
   return { title: "Error Occured", heading: "このページは現在表示できません。", description: "クライアント側でエラーが発生しました。"}
 }
